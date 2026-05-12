84213921bed6c4d15256b11e32629a3553e49f4a
Alvin-24TI <alvin24ti@mahasiswa.pcr.ac.id>
2026-05-13 04:40:01 +0700

    P8
    https://fikri-fmi.notion.site/Pertemuan-7-31554857ba468192886df6ff0b76077a

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
-import "./assets/tailwind.css";
-import Sidebar from "./layouts/Sidebar";
-import Header from "./layouts/Header";
-import PageHeader from "./components/PageHeader";
-import Dashboard from "./pages/Dashboard";
-import Orders from "./pages/Orders";
-import Customers from "./pages/Customers";
-import NotFound from "./pages/NotFound";
-
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import "./assets/tailwind.css";
+
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import PageHeader from "./components/PageHeader";
+import Loading from "./components/Loading";
+
+
+const Dashboard = React.lazy(() => import("./pages/Dashboard"));
+const Orders = React.lazy(() => import("./pages/Orders"));
+const Customers = React.lazy(() => import("./pages/Customers"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+import MainLayouts from "./layouts/MainLayouts";
+
+import AuthLayout from "./layouts/AuthLayouts";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Forgot from "./pages/auth/Forgot";
+
 
 function App() {
 
 
   return (
-    <div id="app-container" className="bg-gray-100 min-h-screen flex">
-      <div id="layout-wrapper" className="flex flex-row flex-1">
-        <Sidebar />
-        <div id="main-content" className="flex-1 p-4">
-          <Header />
-          <Routes>
-            <Route path="*" element={<NotFound />} />
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/customers" element={<Customers />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route element={<MainLayouts />}>
+          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/customers" element={<Customers />} />
+        </Route>
+
+        <Route element={<AuthLayout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot" element={<Forgot />} />
+        </Route>
+      </Routes>
+    </Suspense>
   )
 }
 
